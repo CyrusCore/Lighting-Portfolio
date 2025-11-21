f3780c1df596f9e19a3d2ee4ef1af7521f4e154b
CyrusCore <abramsatria7@gmail.com>
2025-11-21 10:14:56 +0700

    hello

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./*.html"  // This tells it to scan index.html, portfolio.html, etc.
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
